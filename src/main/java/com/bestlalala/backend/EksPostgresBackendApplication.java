ce36b41b588db72ac1d1c25ae081dc1154fdd5af
yeonsu <bestdustn@sookmyung.ac.kr>
2024-11-29 15:41:57 +0900

    Initial Setting

---
+++ src/main/java/com/bestlalala/backend/EksPostgresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bestlalala.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksPostgresBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksPostgresBackendApplication.class, args);
+    }
+
+}
